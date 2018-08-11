5bb5b790c82c730b0b2816d754530c3d5089d1fb
Lee Sargent <leecsargent@gmail.com>
2018-08-10 21:01:16 -0400

    remove dist

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,27 +7,27 @@ import runSequence from 'run-sequence';
 const plugins = gulpLoadPlugins();
 
 const paths = {
-  js: ['./**/*.js', '!dist/**', '!node_modules/**', '!coverage/**'],
+  js: ['./**/*.js', '!build/**', '!node_modules/**', '!coverage/**'],
   nonJs: ['./package.json', './.gitignore', './.env'],
   tests: './server/tests/*.js'
 };
 
-// Clean up dist and coverage directory
+// Clean up build and coverage directory
 gulp.task('clean', () =>
-  del.sync(['dist/**', 'dist/.*', 'coverage/**', '!dist', '!coverage'])
+  del.sync(['build/**', 'build/.*', 'coverage/**', '!build', '!coverage'])
 );
 
-// Copy non-js files to dist
+// Copy non-js files to build
 gulp.task('copy', () =>
   gulp.src(paths.nonJs)
-    .pipe(plugins.newer('dist'))
-    .pipe(gulp.dest('dist'))
+    .pipe(plugins.newer('build'))
+    .pipe(gulp.dest('build'))
 );
 
-// Compile ES6 to ES5 and copy to dist
+// Compile ES6 to ES5 and copy to build
 gulp.task('babel', () =>
   gulp.src([...paths.js, '!gulpfile.babel.js'], { base: '.' })
-    .pipe(plugins.newer('dist'))
+    .pipe(plugins.newer('build'))
     .pipe(plugins.sourcemaps.init())
     .pipe(plugins.babel())
     .pipe(plugins.sourcemaps.write('.', {
@@ -36,15 +36,15 @@ gulp.task('babel', () =>
         return path.relative(file.path, __dirname);
       }
     }))
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('build'))
 );
 
 // Start server with restart on file changes
 gulp.task('nodemon', ['copy', 'babel'], () =>
   plugins.nodemon({
-    script: path.join('dist', 'index.js'),
+    script: path.join('build', 'index.js'),
     ext: 'js',
-    ignore: ['node_modules/**/*.js', 'dist/**/*.js'],
+    ignore: ['node_modules/**/*.js', 'build/**/*.js'],
     tasks: ['copy', 'babel']
   })
 );
@@ -52,7 +52,7 @@ gulp.task('nodemon', ['copy', 'babel'], () =>
 // gulp serve for development
 gulp.task('serve', ['clean'], () => runSequence('nodemon'));
 
-// default task: clean dist, compile js files and copy non-js files.
+// default task: clean build, compile js files and copy non-js files.
 gulp.task('default', ['clean'], () => {
   runSequence(
     ['copy', 'babel']
